5ab3db4b2b80ccbcc100bba6b55dfc3355b15c13
kj-111 <76260068+kj-111@users.noreply.github.com>
2026-05-01 16:24:11 +0200

    Skip netrw in last buffer switch

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,4 +1,23 @@
-vim.keymap.set('n', '<C-i>', '<C-^>')
+local function switch_last_file_buffer()
+  local current = vim.api.nvim_get_current_buf()
+  local buffers = vim.tbl_filter(function(buffer)
+    if buffer.bufnr == current or buffer.loaded ~= 1 or buffer.name == '' then return false end
+    if vim.bo[buffer.bufnr].buftype ~= '' then return false end
+    if vim.bo[buffer.bufnr].filetype == 'netrw' then return false end
+    return true
+  end, vim.fn.getbufinfo({ buflisted = 1 }))
+
+  table.sort(buffers, function(left, right) return (left.lastused or 0) > (right.lastused or 0) end)
+
+  if not buffers[1] then
+    vim.notify('No previous file buffer', vim.log.levels.INFO)
+    return
+  end
+
+  vim.api.nvim_set_current_buf(buffers[1].bufnr)
+end
+
+vim.keymap.set('n', '<C-i>', switch_last_file_buffer)
 vim.keymap.set('n', '<C-e>', function() vim.cmd(vim.bo.filetype == 'netrw' and 'bd' or 'Explore') end)
 vim.keymap.set('n', '<leader>u', '<cmd>Undotree<CR>')
 vim.keymap.set('n', '<leader>v', '<cmd>Markview toggle<CR>')
